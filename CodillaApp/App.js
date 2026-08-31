ca511a14740c978d06360dd45f5ee3432431b219
Aries Andrew Codilla <a.codilla.147416.tc@umindanao.edu.ph>
2026-08-31 21:02:13 +0800

    Fourth Commit
    Changed the company name and altered the BG color a bit.

--- CodillaApp/App.js
+++ CodillaApp/App.js
@@ -4,7 +4,7 @@ import StatCard from './components/StatCard';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>MY CUSTOM DASHBOARD 📊</Text>
+      <Text style={styles.header}>ROLANDO'S COMPANY DASHBOARD 📊</Text>
 
       {/* Rendering multiple cards using props */}
       <StatCard
@@ -31,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dde8ff',
+    backgroundColor: '#cfe0ff',
     padding: 20,
     paddingTop: 60,
   },
